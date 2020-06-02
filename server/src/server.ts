7e37d82ef00a937bb2100e299a1f82b73dd3405b
Pedro Morais <pedromorais2@hotmail.com>
2020-06-02 19:42:49 +0100

    knex migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('hello Pedro');
-  res.json('Hello Pedro Morais');
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
